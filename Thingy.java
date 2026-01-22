a54def320ddd0a08566f9bfc06ad8a4ffb07ae74
Mathieu Bernardin <2551625@edu.vaniercollege.qc.ca>
2026-01-22 14:16:45 -0500

    Initial commit

---
+++ Thingy.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Thingy{
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Please enter a series of ten numbers");
+        for(int i=0;i<10;++i){
+            int num=input.nextInt();
+            if(num%2==0){
+                System.out.println("Entry number "+(i+1)+" is even");
+            }
+            else{
+                System.out.println("Entry number "+(i+1)+" is odd");
+            }
+        }
+        input.close();
+    }
+}
