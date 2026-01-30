1c7b1642cafd41f8e3190592782faecd7830bfff
Mathieu Bernardin <2551625@edu.vaniercollege.qc.ca>
2026-01-30 15:44:17 -0500

    start table array excercises

---
+++ ArrayQuestionsTwoDimensions.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+@SuppressWarnings("unused")
+public class ArrayQuestionsTwoDimensions{
+    public static void main(String[] args) {
+        ArrayQuestionsTwoDimensions thing=new ArrayQuestionsTwoDimensions();
+    }
+    public ArrayQuestionsTwoDimensions(){
+        sumColumns();
+    }
+    private String printTable(int[][] table){
+        String tableString="Visualisation of table:\n";
+        for(int x=0;x<table.length;++x){
+            for(int y=0;y<table.length;++y){
+                tableString+=table[x][y]+" ";
+            }
+            tableString+="\n";
+        }
+        return tableString;
+    }
+    //q2
+    private void sumTable(){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Please enter a number of columns\n> ");
+        int columns=input.nextInt();
+        System.out.print("Please enter a number of rows\n> ");
+        int rows=input.nextInt();
+        int[][] table=new int[columns][rows];
+        for(int x=0;x<table.length;++x){
+            for(int y=0;y<table.length;++y){
+                System.out.print("Please enter a number\n> ");
+                table[x][y]=input.nextInt();
+            }
+        }
+        System.out.println("Table built");
+        input.close();
+        System.out.print(printTable(table));
+        int sum=0;
+        for(int x=0;x<table.length;++x){
+            for(int y=0;y<table.length;++y){
+                sum+=table[x][y];
+            }
+        }
+        System.out.println("Sum of whole table is "+sum);
+    }
+    //q3
+    private void sumRows(){
+        Scanner input=new Scanner(System.in);
+        int[][] table=new int[3][3];
+        for(int x=0;x<table.length;++x){
+            for(int y=0;y<table.length;++y){
+                System.out.print("Please enter a number\n> ");
+                table[x][y]=input.nextInt();
+            }
+        }
+        System.out.println("Table built");
+        input.close();
+        System.out.print(printTable(table));
+        for(int x=0;x<table.length;++x){
+            int sum=0;
+            for(int y=0;y<table.length;++y){
+                sum+=table[x][y];
+            }
+            System.out.println("Sum of row "+x+": "+sum);
+        }
+    }
+    //q4 -- print the sum of the columns of a 2*5 table
+    private void sumColumns(){
+        Scanner input=new Scanner(System.in);
+        int[][] table=new int[2][5];
+        for(int x=0;x<2;++x){
+            for(int y=0;y<5;++y){
+                System.out.print("Please enter a number\n> ");
+                table[x][y]=input.nextInt();
+            }
+        }
+        System.out.println("Table built");
+        input.close();
+        System.out.print(printTable(table));
+        for(int y=0;y<5;++y){
+            int sum=0;
+            for(int x=0;x<2;++x){
+                sum+=table[x][y];
+            }
+            System.out.println("Sum of columns "+y+": "+sum);
+        }
+    }
+}
