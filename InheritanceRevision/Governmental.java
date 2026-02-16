3515d0a185cc4652a2f0ff7f11b2e791c868af0d
m-bernardin <mjrb4mjrb@gmail.com>
2026-02-16 16:07:30 -0500

    restructure inheritancce directory

--- InheritanceRevision/Governmental.java
+++
@@ -1,9 +0,0 @@
-public class Governmental extends Orginization{
-    private int nbEmployees;
-    public int getNbEmployees(){
-        return nbEmployees;
-    }
-    public void setNbEmployees(int nbEmployees){
-        this.nbEmployees = nbEmployees;
-    }
-}
