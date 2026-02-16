1f26b2128c96c43749375d8abd8c7e02e13ac755
m-bernardin <mjrb4mjrb@gmail.com>
2026-02-16 17:43:50 -0500

    input almost done

--- InheritanceRevision/Store/Driver.java
+++ InheritanceRevision/Store/Driver.java
@@ -8,8 +8,10 @@ public class Driver{
         System.out.println("bike info:\n"+bikes+"\ncamera info:\n"+cameras+"\ngroceries info:\n"+groceries+"\nphones info:\n"+phones);
         Scanner input=new Scanner(System.in);
         boolean running=true;
+
         while (running) {
             System.out.print("Create a...\n0. Quit\n1. Store\n2. Bike store\n3.Grocery store\n4. MobileStore\n>");
+            Store store=null;
             switch (input.nextInt()) {
                 case 0:
                     running=false;
@@ -18,8 +20,7 @@ public class Driver{
                 case 1:
                     input.nextLine();
                     System.out.print("Creating a store...\nPlease enter a name\n>");
-                    Store store=new Store(input.nextLine());
-                    System.out.println(store);
+                    store=new Store(input.nextLine());
                     break;
                 case 2:
                     input.nextLine();
@@ -27,13 +28,30 @@ public class Driver{
                     String name=input.nextLine();
                     System.out.print("Please enter the number of bike brands this store carries\n>");
                     int nbBikeBrands=input.nextInt();
-                    BikeStore bikeStore;
                     System.out.print("Does this store sponsor a club? (Y/n)\n>");
-                    if(input.next().equals("Y"))bikeStore=new BikeStore(name, nbBikeBrands, true);
-                    else bikeStore=new BikeStore(name, nbBikeBrands, false);
-                    System.out.println(bikeStore);
+                    if(input.next().equals("Y"))store=new BikeStore(name, nbBikeBrands, true);
+                    else store=new BikeStore(name, nbBikeBrands, false);
                     break;
+                case 3:
+                    input.nextLine();
+                    System.out.print("Creating a grocery store...\nPlease enter the name\n>");
+                    name=input.nextLine();
+                    System.out.print("Please enter the annual revenue of this store\n>");
+                    int annualRevenue=input.nextInt();
+                    System.out.print("Is this store independent? (Y/n)\n>");
+                    if(input.next().equals("Y"))store=new GroceryStore(name, annualRevenue, true);
+                    else store=new GroceryStore(name, annualRevenue, false);
+                    break;
+                case 4:
+                    input.nextLine();
+                    System.out.print("Creating a mobile store...\nPlease enter the name\n>");
+                    name=input.nextLine();
+                    System.out.print("Please enter the annual sales of this store\n>");
+                    int annualSales=input.nextInt();
+                    int[] monthlySales=new int[12];
+                    for(in)
             }
+            System.out.println(store);
         }
     }
 }
